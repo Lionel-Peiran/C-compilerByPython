360a6f41e2b8fe384df3a48ecbe44e26b567bfd9
yyjxx2010xyu <yyjxx2010xyu@gmail.com>
2020-12-07 19:01:57 +0800

    add comment

--- Tools.py
+++ Tools.py
@@ -7,7 +7,17 @@ from Grammar import PP, Book
 
 Item = namedtuple('Item', 'pid dot ahead')
 
+'''
+    Input:
+        Grammar_Table   产生式
+    
+    Output:
+        NullS           NULLABLE集
+        可以直接推出空串的集合
 
+    Func:
+        求解Nullable集
+'''
 def Nullable_Set(Grammar_Table):
     NullS = set()
     while True:
@@ -15,6 +25,7 @@ def Nullable_Set(Grammar_Table):
         for lhs, *rhs in Grammar_Table:
             if lhs in NullS:
                 continue
+            #   A -> ε  |   A -> BC...,  B,C,... in NullS
             if (len(rhs) == 1 and rhs[-1] == Empty_Symbol) or all(map(lambda i: i in NullS, rhs)):
                 NullS.add(lhs)
                 Flag = True
@@ -22,6 +33,17 @@ def Nullable_Set(Grammar_Table):
             break
     return NullS
 
+'''
+    Input:
+        Grammar_Table   产生式
+    
+    Output:
+        TerminalS       终结符
+        即不能单独出现在推导式左边的符号，也就是说终结符不能再进行推导。
+    
+    Func:
+        求解Terminal集
+'''
 def Terminal_Set(Grammar_Table):
     TerminalS = set()
     NotTerminalS = set()
@@ -32,20 +54,44 @@ def Terminal_Set(Grammar_Table):
     TerminalS = TerminalS - NotTerminalS
     return TerminalS
 
-def First_Closure(ps, first, terminal, nullable, left=None, follow=None):
-    res = set()
-    for b in ps:
-        if b in terminal:
-            res.add(b)
-            return res
-        else:
-            res = res.union(first[b])
-            if b not in nullable:
-                return res
-    if left and follow:
-        res.add(follow[left])
-    return res
+'''
+    Input:
+        terms           式子右边的项
+        FirstS          First集
+        TerminalS       Terminal集
 
+    
+    Output:
+        TerminalS       终结符
+
+    Func:
+        求解单独一个式子的First集
+'''
+def First_Closure(terms, FirstS, TerminalS, NullS):
+    curClosure = set()
+    for term in terms:
+        if term in TerminalS:
+            curClosure.add(term)
+            return curClosure
+        else:
+            curClosure = curClosure.union(FirstS[term])
+            if term not in NullS:
+                return curClosure
+    return curClosure
+
+
+'''
+    Input:
+        Grammar_Table   产生式
+        TerminalS       Terminal集
+        NullS           Nullable集
+    
+    Output:
+        FirstS          First集
+
+    Func:
+        求解整个产生式的First集
+'''
 def First_Set(Grammar_Table, Terminal, NullS):
     First = defaultdict(set)
     while True:
@@ -56,11 +102,25 @@ def First_Set(Grammar_Table, Terminal, NullS):
             break
     return First
 
+
+'''
+    Input:
+        Grammar_Table   产生式
+        FirstS          First集
+        TerminalS       Terminal集
+        NullS           Nullable集
+    
+    Output:
+        FollowS         Follow集
+
+    Func:
+        求解整个产生式的Follow集
+'''
 def Follow_Set(Grammar_Table, FirstS, Terminal, NullS):
     Follow = defaultdict(set)
-    changing = True
-    while changing:
-        changing = False
+
+    while True:
+        Flag = False
         for lhs, *rhs in Grammar_Table:
             cur = Follow[lhs]
             for term in rhs[::-1]:
@@ -69,123 +129,125 @@ def Follow_Set(Grammar_Table, FirstS, Terminal, NullS):
                 else:
                     if cur - Follow[term]:
                         Follow[term] = cur.union(Follow[term])
-                        changing = True
+                        Flag = True
                     if term in NullS:
                         cur = cur.union(FirstS[term])
                     else:
                         cur = FirstS[term]
+        if not Flag:
+            break
 
     return Follow
 
-def Nullable_Closure(ps, nullable):
-    return all(map(lambda i: i in nullable, ps))
 
-def Calc_Closure(iniClosure, Grammar_Table, FirstS, TerminalS, NullS):
+'''
+    Input:
+        terms           式子右边的项
+        NullS           Nullable集
+   
+    Output:
+        Null            Nullable集
+
+    Func:
+        求解单独一个式子的Nullable集
+'''
+def Nullable_Closure(terms, NullS):
+    return all(map(lambda i: i in NullS, terms))
+
+
+'''
+    Input:
+        terms           式子右边的项
+        Extended_Table  增广产生式
+        FirstS          First集
+        TerminalS       Terminal集
+        NullS           Nullable集
+    
+    Output:
+        Closure         求该项的闭包
+
+    Func:
+        求出一个项集的闭包
+'''
+def Calc_Closure(items, Extended_Table, FirstS, TerminalS, NullS):
     LookUp = defaultdict(list)
-    for id, (lhs, *rhs) in enumerate(Grammar_Table):
+    for id, (lhs, *rhs) in enumerate(Extended_Table):
         LookUp[lhs].append(id)
 
-    curClosure = set(iniClosure)
+    curClosure = set(items)
 
-    changing = True
-    while changing:
-        changing = False
-
-        for term in curClosure.copy():
-            lhs, *rhs = Grammar_Table[term.pid]
-
-            if len(rhs) > term.dot and rhs[term.dot] not in TerminalS:
-
-                curSel = First_Closure(rhs[term.dot+1:], FirstS, TerminalS, NullS)
-
-                if Nullable_Closure(rhs[term.dot+1:], NullS):
-                    curSel = curSel.union(term.ahead)
-
-                for pid in LookUp[rhs[term.dot]]:
-                    term = Item(pid, 0, tuple(curSel))
+    while True:
+        Flag = False
+        for s in curClosure.copy():
+            lhs, *rhs = Extended_Table[s.pid]
+            # 对于每一个形如 A -> a*Bb, c 的项
+            if len(rhs) > s.dot and rhs[s.dot] not in TerminalS:
+                fs = First_Closure(rhs[s.dot+1:], FirstS, TerminalS, NullS)
+                if Nullable_Closure(rhs[s.dot+1:], NullS): 
+                    fs = fs.union(s.ahead)
+                for pid in LookUp[rhs[s.dot]]:
+                    term = Item(pid, 0, tuple(fs))
                     if term not in curClosure:
                         curClosure.add(term)
-                        changing = True
+                        Flag = True
+        if not Flag:
+            break
 
     return curClosure
 
-def closure(items, productions, FIRST, TERMINAL, Nullable):
-    LookUp = defaultdict(list)
-    for id, (lhs, *right) in enumerate(productions):
-        LookUp[lhs].append(id)
 
-    res = set(items)
-    changing = True
-    while changing:
-        changing = False
-        for s in res.copy():
-            lhs, *right = productions[s.pid]
-            # 对于每一个形如 A -> a*Bb, c 的项
-            if len(right) > s.dot and right[s.dot] not in TERMINAL: # 项可展开
-                fs = First_Closure(right[s.dot+1:], FIRST, TERMINAL, Nullable)
-                if Nullable_Closure(right[s.dot+1:], Nullable):  # Bc可为空
-                    fs = fs.union(s.ahead)
-                for pid in LookUp[right[s.dot]]:
-                    t = Item(pid, 0, tuple(fs))
-                    if t not in res:
-                        res.add(t)
-                        changing = True
-
-    return res
-
-
-def goto(items, X, productions, FIRST, TERMINAL, Nullable):
+def Goto(items, X, Extended_Table, FirstS, TerminalS, NullS):
     res = set()
     for item in items:
-        p = productions[item.pid]
-        if (item.dot+1) < len(p) and p[item.dot+1] == X:
-            c = closure({Item(item.pid, item.dot+1, item.ahead)}, productions, FIRST, TERMINAL, Nullable)
-            res = res.union(c)
+        pos = Extended_Table[item.pid]
+        if (item.dot+1) < len(pos) and pos[item.dot+1] == X:
+            clo = Calc_Closure({Item(item.pid, item.dot+1, item.ahead)}, Extended_Table, FirstS, TerminalS, NullS)
+            res = res.union(clo)
     return res
 
 
-def LR1_parse_tableOld(productions, FIRST, TERMINAL, Nullable, start_symbol='<程序>'):
+def LR1_Table(Extended_Table, FirstS, TerminalS, NullS, Start_Symbol):
 
-    productions = productions[:]
-    productions.insert(0, ('S', start_symbol))  # 增广文法
-    for i in range(len(productions)):
-        if productions[i][-1] == 'ε' and len(productions[i]) == 2:
-            productions[i] = (productions[i][0],)
-    TERMINAL = TERMINAL.copy()
-    TERMINAL.add('$')
+    Extended_Table = Extended_Table[:]
+    Extended_Table.insert(0, ('S', Start_Symbol))
+    for i in range(len(Extended_Table)):
+        if Extended_Table[i][-1] == 'ε' and len(Extended_Table[i]) == 2:
+            Extended_Table[i] = (Extended_Table[i][0],)
+    TerminalS = TerminalS.copy()
+    TerminalS.add('$')
 
 
     Action_Table = defaultdict(dict)
     Goto_Table = defaultdict(dict)
     
-    #Action_Table = defaultdict(dict)
-    c = closure({Item(0, 0, ('$',))}, productions, FIRST, TERMINAL, Nullable)
-    states = [c]  # closure
-    unresolve = [0]  # closure id that need resolve
-    while unresolve:
-        cid = unresolve.pop()
-        print(len(unresolve))
-        for item in states[cid]:
-            left, *right = productions[item.pid]
-            if len(right) == item.dot:  # 规约项
+    c = Calc_Closure({Item(0, 0, ('$',))}, Extended_Table, FirstS, TerminalS, NullS)
+    State = [c] 
+    Queue = [0] 
+    while Queue:
+        cid = Queue.pop()
+        print(len(Queue))
+        for item in State[cid]:
+            lhs, *rhs = Extended_Table[item.pid]
+            if len(rhs) == item.dot:
                 for ahead in item.ahead:
-                    Action_Table[cid][ahead] = (ActionState.Reduce, item.pid)  # 设置action为”规约“
+                    Action_Table[cid][ahead] = (ActionState.Reduce, item.pid)  
                     if item.pid == 0:
-                        Action_Table[cid][ahead] = (ActionState.Accept, item.pid)  # 设置action为”接受“
-            elif right[item.dot] not in Action_Table[cid] and (right[item.dot] not in Goto_Table[cid]):  # 未处理的移进项目和待归约项目
-                c = goto(states[cid], right[item.dot],
-                         productions, FIRST, TERMINAL, Nullable)
+                        Action_Table[cid][ahead] = (ActionState.Accept, item.pid)
+            elif rhs[item.dot] not in Action_Table[cid] and (rhs[item.dot] not in Goto_Table[cid]): 
+                c = Goto(State[cid], rhs[item.dot],
+                         Extended_Table, FirstS, TerminalS, NullS)
                 try:
-                    index = states.index(c)
+                    index = State.index(c)
                 except ValueError:
-                    states.append(c)
-                    index = len(states)-1
-                    unresolve.append(index)
-                if right[item.dot] in TERMINAL:
-                    Action_Table[cid][right[item.dot]] = (ActionState.Shift, index)  # 设置action为”移进“
+                    State.append(c)
+                    index = len(State) - 1
+                    Queue.append(index)
+                if rhs[item.dot] in TerminalS:
+                    Action_Table[cid][rhs[item.dot]] = (ActionState.Shift, index) 
                 else:
-                    Goto_Table[cid][right[item.dot]] =  index
-    return productions, Action_Table, Goto_Table
+                    Goto_Table[cid][rhs[item.dot]] =  index
+                    
+    return Extended_Table, Action_Table, Goto_Table
 
 
 def Load_Table():
@@ -207,5 +269,5 @@ if __name__ == '__main__':
     FIRST = First_Set(S, TERMINAL, Nullable)
     FOLLOW =  Follow_Set(S,FIRST, TERMINAL, Nullable)
 
-    Extended_Table, Action_Table, Goto_Table = LR1_parse_tableOld(S, FIRST, TERMINAL, Nullable, start_symbol='translation_unit')
+    Extended_Table, Action_Table, Goto_Table = LR1_Table(S, FIRST, TERMINAL, Nullable, Start_Symbol='translation_unit')
     pickle.dump((Extended_Table, dict(Action_Table), dict(Goto_Table)), open('parse_table.dat', 'wb'))
